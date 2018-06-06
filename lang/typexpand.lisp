05dbd839172d6eb6dfc78cc4e53238f95c0c7369
David Gu <macdavid313@gmail.com>
2018-06-07 00:31:51 +0800

    support more Lisps (#7)
    Support AllegroCL, ECl, and LispWorks.

--- lang/typexpand.lisp
+++ lang/typexpand.lisp
@@ -22,25 +22,51 @@ TYPEXPAND
 
 
 (defun typexpand (type &optional env)
-  (declare (type (or symbol cons) type))
+  (declare (type (or symbol cons) type)
+           #+(or ecl lispworks) (ignore env))
   #+(or ccl cmucl sbcl)
-  (introspect-environment:typexpand type env)
+  (introspect-environment:typexpand type env)  
   #+abcl
   (system::expand-deftype type)
+  #+allegro
+  (excl::deftype-expand type env)
   #+clisp
-  (ext:type-expand type env))
+  (ext:type-expand type env)
+  #+ecl
+  (si::expand-deftype type)
+  #+lispworks
+  (type::expand-user-type type))
+
+#+ecl
+(defun expand-deftype-1 (type)
+  "This function largely copies from ECL's expand-deftype"
+  (let (base args)
+    (if (atom type)
+        (setf base type
+              args nil)
+        (setf base (car type)
+              args (cdr type)))
+    (let ((expander (system::get-sysprop base 'system::DEFTYPE-DEFINITION)))
+      (funcall expander type))))
 
 (defun typexpand-1 (type &optional env)
-  (declare (type (or symbol cons) type))
+  (declare (type (or symbol cons) type)
+           #+(or ecl lispworks) (ignore env))
   #+(or ccl cmucl sbcl)
   (introspect-environment:typexpand-1 type env)
   #+abcl
   (system::expand-deftype type)
+  #+allegro
+  (excl::deftype-expand-1 type env)
   #+clisp
-  (ext:type-expand-1 type))
+  (ext:type-expand-1 type)
+  #+ecl
+  (expand-deftype-1 type)
+  #+lispworks
+  (type::expand-user-type-1 type))
 
 
-#-(or abcl ccl clisp cmucl sbcl)
+#-(or abcl allegro ccl clisp cmucl ecl lispworks sbcl)
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (error "no known implementation of TYPEXPAND on this platform,
   cannot compile CL-PARAMETRIC-TYPES"))
